95c892679a47749a16ad57bd08523e1036313446
Quang <tavinhquang04506@gmail.com>
2026-03-26 20:58:55 +0700

    fix: configure for Render.com deployment - fix devDependencies install, remove tracked dist/, update render.yaml

--- backend/dist/src/routes/checkout.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const checkout_controller_1 = require("../controllers/checkout.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.use(auth_middleware_1.authenticate);
-router.post('/', checkout_controller_1.checkout);
-exports.default = router;
